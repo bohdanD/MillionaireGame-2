2f5ac20c6670e5045f76691f7f77a4e2ec11db12
Bohdan Dorosh <doroshbohdan1@gmail.com>
2018-12-30 13:58:07 +0200

    Remove usings.

--- MillionaireGame.Question/MillionaireGame.Question.Domain/Answer.cs
+++ MillionaireGame.Question/MillionaireGame.Question.Domain/Answer.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace MillionaireGame.Question.Domain
+﻿namespace MillionaireGame.Question.Domain
 {
     public class Answer
     {
