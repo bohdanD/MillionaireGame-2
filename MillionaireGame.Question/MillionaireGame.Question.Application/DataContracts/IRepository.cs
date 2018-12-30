1e87a0d2327f7c9414eddfd61ef9e3f7cb922e0d
Bohdan Dorosh <doroshbohdan1@gmail.com>
2018-12-30 15:41:03 +0200

    Add Data Contract for Question.

---
+++ MillionaireGame.Question/MillionaireGame.Question.Application/DataContracts/IRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace MillionaireGame.Question.Application.DataContracts
+{
+    public interface IRepository<T> where T : class
+    {
+        T GetSingle(Expression<Func<T, bool>> expression);
+        IEnumerable<T> GetMany(Expression<Func<T, bool>> expression);
+    }
+}
