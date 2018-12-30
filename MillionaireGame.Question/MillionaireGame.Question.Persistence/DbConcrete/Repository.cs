16ca5a912899aa2cc89714dfe92fb23a0d122523
Bohdan Dorosh <doroshbohdan1@gmail.com>
2018-12-30 15:41:27 +0200

    Add Persistace.

---
+++ MillionaireGame.Question/MillionaireGame.Question.Persistence/DbConcrete/Repository.cs
@@ -0,0 +1,28 @@
+﻿using MillionaireGame.Question.Application.DataContracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace MillionaireGame.Question.Persistence.DbConcrete
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly QuestionDbContext _questionDbContext;
+
+        public Repository(QuestionDbContext questionDbContext)
+        {
+            _questionDbContext = questionDbContext;
+        }
+
+        public IEnumerable<T> GetMany(Expression<Func<T, bool>> expression)
+        {
+            return _questionDbContext.Set<T>().Where(expression).AsEnumerable();
+        }
+
+        public T GetSingle(Expression<Func<T, bool>> expression)
+        {
+            return _questionDbContext.Set<T>().Where(expression).FirstOrDefault();
+        }
+    }
+}
---
+++ MillionaireGame.Question/MillionaireGame.Question.Persistence/QuestionDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MillionaireGame.Question.Domain;
+
+namespace MillionaireGame.Question.Persistence
+{
+    public class QuestionDbContext : DbContext
+    {
+        public DbSet<Domain.Question> Questions { get; set; }
+        public DbSet<Answer> Answers { get; set; }
+        public DbSet<Complexity> Complexities { get; set; }
+
+    }
+}
