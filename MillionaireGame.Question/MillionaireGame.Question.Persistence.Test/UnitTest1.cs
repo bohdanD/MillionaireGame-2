7c7b8ae0f6aada4a2720d1d90d5361c4f36d9edf
Bohdan Dorosh <doroshbohdan1@gmail.com>
2018-12-31 15:55:29 +0200

    Add Persistence test project.

---
+++ MillionaireGame.Question/MillionaireGame.Question.Persistence.Test/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace MillionaireGame.Question.Persistence.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            // TODO: Add Persistence tests when some kind of Data Storage will be added.
+        }
+    }
+}
